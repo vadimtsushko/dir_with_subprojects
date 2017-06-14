ec07021b6f85c7670a456455b1c05924e8e27e61
Vadim Tsushko <vadimtsushko@gmail.com>
2017-06-14 13:36:17 +0500

    Some new file

---
+++ project3/bin/some.dart
@@ -0,0 +1,9 @@
+import 'package:project1/project1.dart' as project1;
+import 'package:project2/project2.dart' as project2;
+import 'package:project3/project3.dart' as project3;
+
+main() {
+  print(new project1.Awesome().isAwesome);
+  print(project2.calculate());
+  print(project3.calculate());
+}
